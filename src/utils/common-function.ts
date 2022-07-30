13e3475616036471b09987c7ccc35613eafff70f
Tran Trong Duc <shyanime92@gmail.com>
2022-07-31 00:51:46 +0700

    feat: remove moment

--- src/utils/common-function.ts
+++ src/utils/common-function.ts
@@ -1,15 +1,15 @@
+import dayjs from 'dayjs';
 import { identity, pickBy } from 'lodash';
-import moment from 'moment';
 import querystring, { StringifyOptions } from 'query-string';
 
 import { FORMAT_DATE_DD_MM_YYYY } from '@/contants/common.constants';
 
 export const formatDateRequest = (date: string | Date | undefined) => {
-  return moment(date).format().toString();
+  return dayjs(date).format().toString();
 };
 
 export const formatRangeDate = (date: Date | string) => {
-  return moment(date, FORMAT_DATE_DD_MM_YYYY).toISOString();
+  return dayjs(date, FORMAT_DATE_DD_MM_YYYY).toISOString();
 };
 
 export function toQuery(
